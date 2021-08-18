31ffba217c048053c16a2c9fad8ffd7c2ab4bca3
Callum Bond <c.bond@base2services.com>
2021-08-18 11:47:42 +1000

    working

--- lib/services/elasticsearchservice.py
+++ lib/services/elasticsearchservice.py
@@ -8,6 +8,7 @@ class Elasticsearchservice:
         self.identifiers = []
         self.templates = {
             'identifier': 'Domain',
+            'identifier': 'AccountId',
             'cfn-monitor': 'ElasticSearch',
             'cfn-guardian': 'ElasticSearch'
         }
@@ -16,30 +17,18 @@ class Elasticsearchservice:
     def get_resources(self):
         try:
             client = boto3.client('es', region_name=self.region)
-            get_domains = client.list_domain_names()                    
-            for item in get_domains['DomainNames']:     
-                list_domains = client.describe_elasticsearch_domains(  
-                    DomainNames=[
-                        item['DomainName']                      
-                    ])
+            get_domains = client.list_domain_names()       
+            
+            for item in get_domains['DomainNames']:
+                account_id = boto3.client("sts").get_caller_identity()["Account"]
+                self.identifiers.extend([{
+                'id': {
+                    'Id': account_id,
+                    'AccountId': account_id,
+                    'Domain': item['DomainName']
+                }
+            }])             
+
         except Exception as e: 
             print('ERROR'.ljust(7) + self.region.ljust(16) + self.name.ljust(19) + str(e), flush=True)
             pass
-        
-        for item in list_domains['DomainStatusList']:
-            account_id = boto3.client("sts").get_caller_identity()["Account"]
-            self.identifiers.extend([{
-                'id': {
-                    'Id:': account_id,
-                    'Domain': 'hello'
-                }
-            }])
-
-
-# ElasticSearch:
-#   - Domain: tg-prod-elastic-v6
-#     Id: '223829094007'
-
-#   ElasticSearch:
-#   - Id: !!python/tuple
-#     - 223829094007/tg-prod-elastic-v6
